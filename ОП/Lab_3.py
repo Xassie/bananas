5184699c838e098aadc41939a99f2e82d49d0382
Xassie <XasWorks@gmail.com>
2019-09-26 13:37:26 +0700

    almost

---
+++ ОП/Lab_3.py
@@ -0,0 +1,7 @@
+from random import randint
+import argparse
+
+
+def main():
+     parser = argparse.parser(desctiption='You are allowed to enter '
+                                          'the amount of generated numbers')
