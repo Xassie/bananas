6f321f4d7800f2a3f7145ee303b4f21e69ca3a65
Xassie <XasWorks@gmail.com>
2019-10-15 10:15:09 +0700

    Literally not ideal

--- ОП/Lab_3.py
+++ ОП/Lab_3.py
@@ -9,43 +9,45 @@ from random import randint   # to fill the source
 import argparse   # to make cmd useful
 
 
+def positive(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive"
+                                         "int value" % value)
+    return ivalue
+
+
 def main():
     # cmd usage
     parser = argparse.ArgumentParser(description="You're allowed to enter the "
                                                  "amount of generated numbers")
-    parser.add_argument('-n', action='store', dest='N', default=None,
-                        type=int, help='The amount of elements in the list')
-    R = vars(parser.parse_args())
-    N = R['N']
-    del parser, R
+    parser.add_argument('-n', action='store', dest='n', default=None,
+                        type=positive, help='Amount of elements in the list')
+    parser.add_argument('-r', action='store', dest='r', default=50,
+                        type=positive, help='What\'s the max random number')
+
+    R = parser.parse_args()
+    N, r = R.n, R.r
 
     # checking the input
     if not N:
         while True:
             try:
                 N = int(input('Enter what length this list should be.\n'))
+                if N <= 0:
+                    raise ValueError
                 break
             except ValueError:
-                print('Make sure that you\'ve entered integer')
+                print('Make sure that you\'ve entered pos integer')
 
-    source = []
+    source = tuple([randint(0, r) for i in range(0, N)])
     similarities = []
 
-    # filling the list
-    for i in range(0, N):
-        source.append(randint(0, 100))
+    for i in source:
+        if i not in similarities:
+            similarities.append(i)
 
-    print(source, '\n')
-
-    # getting similar numbers
-    while (len(source) != 0):
-        check = source.pop(0)
-        if (check in source):
-            similarities.append(check)
-            while check in source:
-                source.remove(check)
-
-    print(similarities)
+    print(source, '\n', similarities)
 
 
 if __name__ == '__main__':
