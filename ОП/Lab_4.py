09cd0bb3783588c9518960c61820dd1b2bb3d8d3
Xassie <XasWorks@gmail.com>
2019-11-21 12:08:44 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- ОП/Lab_4.py
+++ ОП/Lab_4.py
@@ -1,32 +1,34 @@
 import argparse
+from random import randint as rand
 
+def quicks(arr):
+    """This module executes quick sort of the list.
+        That's pretty much detailed."""
+    if arr: 
+        return quicks([x for x in arr if x<arr[0]]) + \
+               [x for x in arr if x == arr[0]] + \
+               quicks([x for x in arr if x>arr[0]])
 
-def quick_sort(arr):
-    if arr: return quick_sort([x for x in arr if x<arr[0]]) + \
-                   [x for x in arr if x==arr[0]] + \
-                   quick_sort([x for x in arr if x>arr[0]])
     return []
 
+def cmd():
+        parser = argparse.ArgumentParser(description='Enter the list in quotes.')
+        parser.add_argument('N', action='store', help='List separated by spaces.')
+        R = parser.parse_args()
+        return R.N.split()
+
+def checkitem(item):
+    try:
+        item = [int(i) for i in item]
+        return item
+    except ValueError:
+        print('Error: your list has to contain only integers')
 
 def main():
-    parser = argparse.ArgumentParser(description='Enter the list')
-    parser.add_argument('-n', action='store', dest='N', default=None,
-                        type=int, help='List separated by spaces')
-    R = vars(parser.parse_args())
-    n = R['N']
+    num = checkitem(cmd())
+    if num:
+        print(quicks(num))
 
-    if not n:
-        while True:
-            try:
-                n = input('').split()
-                n = [int(i) for i in n]
-                break
-
-            except ValueError:
-                print('Numbers!')
-
-
-        print(quick_sort(n))
 
 if __name__ == '__main__':
     main()
