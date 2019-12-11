1d094de520ac64a7dac2dd4144d7d5f01bab6cb5
Xassie <XasWorks@gmail.com>
2019-12-11 23:33:22 +0700

    I think it's done

--- ОП/Lab_5.py
+++ ОП/Lab_5.py
@@ -7,8 +7,8 @@ import argparse
 
 SHIFT = 3
 CODES = {
-    'en' = 26
-    'ru' = 33
+    'en' : 26,
+    'ru' : 33,
 }
 
 
@@ -25,51 +25,60 @@ def cmd():
     return R.N.lower()
 
 def decode(ccode, slide, lang):
-    alp = CODES[lang]
-    ccode -= slide + SHIFT
-    if ccode <= 0:
-        ccode += alp
-    return chr(ccode + slide)   
+    if slide:
+        alp = CODES[lang]
+        ccode -= slide - SHIFT
+        if ccode >= alp:
+            ccode -= alp
+    return chr(ccode + slide)
     
 
 def test_decoding():
-    pass
+    assert decode(102, 97, 'en') == 'i'
+    assert decode(76, 65, 'en') == 'O'
+    assert decode(1053, 1040, 'ru') == 'Р'
 
 def encode(ccode, slide, lang):
     if slide:
         alp = CODES[lang]
-        ccode += SHIFT - slide
-        if ccode >= alp:
-            ccode -= alp
+        ccode -= SHIFT + slide
+        if ccode <= 0:
+            ccode += alp
     return chr(ccode + slide)
 
 def test_encoding():
-    pass
+    assert encode(84, 65, 'en') == 'Q'
+    assert encode(104, 97, 'en') == 'e'
+    assert encode(1089, 1072, 'ru') == 'о'
+    assert encode(99, 97, 'en') == 'z'
 
 def determine(character):
 
     code = ord(character)
     if (65 <= code <= 90):
-        lan = en
+        lan = 'en'
         slide = 65
-    elif (97 <= code <= 122)
-        lan = en
+    elif (97 <= code <= 122):
+        lan = 'en'
         slide = 97
     elif (1040 <= code <= 1071):
-        lan = ru
+        lan = 'ru'
         slide = 1040
     elif (1072 <= code <= 1103):
-        lan = ru
+        lan = 'ru'
         slide = 1072
     else:
         lan = None
         slide = 0
     
-    return code, lan, slide
+    return code, slide, lan
 
 
 def test_determination():
-    pass
+    assert determine('T') == (84, 65, 'en')
+    assert determine('h') == (104, 97, 'en')
+    assert determine('с') == (1089, 1072, 'ru')
+    assert determine('c') == (99, 97, 'en')
 
 def read():
     with open('text.txt', "r") as file:
@@ -83,14 +92,15 @@ def write(res):
 def main():
     message = read()
     result = ''
-    for i in message:
-        result += OPTION[cmd()](i)
+    for line in message:
+        for i in line:
+            result += OPTION[cmd()](*determine(i))
     write(result)
 
 
 OPTION = {
-    'decode' = decode
-    'encode' = encode
+    'decode' : decode,
+    'encode' : encode,
 }
 
 if __name__ == "__main__":
